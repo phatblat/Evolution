ae7d0422880f9523473f9bdfcbe33230c63b01b9
Ben Chatelain <ben@octop.ad>
2018-05-18 21:21:37 -0600

    🚀 Add deploy task

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,23 @@
  * Evolution
  */
 
+import at.phatbl.shellexec.ShellExec
+
 plugins {
     id("at.phatbl.clamp") version "1.1.0"
     id("at.phatbl.shellexec") version "1.1.3"
+}
+
+val crashlyticsSubmitPath: String by project
+val crashlyticsApiKey: String by project
+val crashlyticsBuildSecret: String by project
+val crashlyticsGroup: String by project
+val ipaPath: String by project
+
+tasks {
+    val deploy by tasks.creating(ShellExec::class.java) {
+        description = "Deploys the app to Crashlytics"
+        group = "Evolution"
+        command = "echo $crashlyticsSubmitPath $crashlyticsApiKey $crashlyticsBuildSecret -ipaPath $ipaPath -groupAliases $crashlyticsGroup -notesPath RELEASENOTES.md -notifications YES"
+    }
 }
